a2f659552d0cc2b8a24fcda3c0c87972f158a568
Mahmoud Dahy <dahym2028@gmail.com>
2025-10-23 21:47:49 +0300

    create and finish LocalStorageService using hive

---
+++ lib/features/auth/data/service/local_storage.dart
@@ -0,0 +1,51 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:suits/core/constant/app_constant.dart';
+
+
+/*
+ * LocalStorageService class
+ * manages local storage of user data using Hive
+ * provides methods to save, retrieve, and clear user data
+ * checks if a user is logged in
+ * initializes Hive and opens the necessary box
+ */
+class LocalStorageService {
+  static Future<void> init() async {
+    await Hive.initFlutter();
+    await Hive.openBox(boxName);
+  }
+
+  static Future<void> saveUserData({
+    required String uid,
+    required String email,
+    String? name,
+    String? photoUrl,
+  }) async {
+    var box = Hive.box(boxName);
+    await box.put('uid', uid);
+    await box.put('email', email);
+    await box.put('name', name ?? '');
+    await box.put('photoUrl', photoUrl ?? '');
+    await box.put('isLoggedIn', true);
+  }
+
+  static bool isLoggedIn() {
+    var box = Hive.box(boxName);
+    return box.get('isLoggedIn', defaultValue: false);
+  }
+
+  static Map<String, dynamic> getUserData() {
+    var box = Hive.box(boxName);
+    return {
+      'uid': box.get('uid', defaultValue: ''),
+      'email': box.get('email', defaultValue: ''),
+      'name': box.get('name', defaultValue: ''),
+      'photoUrl': box.get('photoUrl', defaultValue: ''),
+    };
+  }
+
+  static Future<void> clearUserData() async {
+    var box = Hive.box(boxName);
+    await box.clear();
+  }
+}
