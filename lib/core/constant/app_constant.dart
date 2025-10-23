8335a175c400d4b48fb3c4b1ababd3e6fe4440a6
Mahmoud Dahy <dahym2028@gmail.com>
2025-10-23 21:35:47 +0300

    Refactor app constants for improved organization and readability

--- lib/core/constant/app_constant.dart
+++ lib/core/constant/app_constant.dart
@@ -3,6 +3,16 @@ const String boxName = 'userBox';
 const int primaryColor = 0xffDD8560;
 const int splashColor = 0xff676767;
 const int scafoldColor = 0xffF4F5F7;
+const int secodaryColor = 0xff4E6542;
 
+const String fontFamily = 'Poppins';
+const String fontFamily2 = 'Montserrat';
+
+const double defaultPadding = 16.0;
+const double defaultHieghtOnbourding = .7;
+const double defaultCirclerpadding = 24.0;
+const double spacebetweenSections = 32;
 
 const String splash = '/';
+const String root = '/root';
+const String getStarted = '/getStarted';
