14e3ebf63e320c3d1de9a1e07d13e2786b9a4309
Mahmoud Dahy <dahym2028@gmail.com>
2025-10-23 21:48:27 +0300

    call some features

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:suits/core/routes/app_routes.dart';
+import 'package:suits/features/auth/data/service/local_storage.dart';
+import 'package:suits/firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await LocalStorageService.init();
   runApp(const Suits());
 }
 
@@ -9,6 +16,9 @@ class Suits extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
+    return  MaterialApp.router(
+      routerConfig: AppRoutes.router,
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
