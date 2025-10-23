0a922f3f2b537a6b616fefe65d6202eca313b175
Mahmoud Dahy <dahym2028@gmail.com>
2025-10-23 21:41:05 +0300

    create assets filw and add all assets

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  static const String imagesCircle = "assets/images/circle.png";
+
+  static const String imagesGetStarted1 = "assets/images/getStarted1.png";
+
+  static const String imagesGetStarted2 = "assets/images/getStarted2.png";
+
+  static const String imagesGetStarted3 = "assets/images/getStarted3.png";
+
+  static const String imagesSuit1 = "assets/images/suit1.jpg";
+
+  static const String imagesSuit2 = "assets/images/suit2.jpg";
+
+  static const String imagesSuit3 = "assets/images/suit3.jpg";
+
+  static const String imagesSuits = "assets/images/suits.png";
+
+  static const String imagesSuitsLogo = "assets/images/suitsLogo.svg";
+
+  static const String imagesGroup2 = "assets/images/Group 2.png";
+
+  static const String curve = "assets/images/curve.png";
+}
