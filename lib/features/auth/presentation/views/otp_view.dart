bd7c69e2436c12aac14f3324dc5efb4f647dcd8f
Mahmoud Dahy <dahym2028@gmail.com>
2025-10-25 15:19:46 +0300

    create OtpView

---
+++ lib/features/auth/presentation/views/otp_view.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:suits/core/constant/app_constant.dart';
+import 'package:suits/core/utils/app_text_style.dart';
+import 'package:suits/core/widgets/custom_app_bar.dart';
+import 'package:suits/core/widgets/custom_button.dart';
+
+import 'widgets/custom_pin_code_text_field.dart';
+
+class OtpView extends StatelessWidget {
+  const OtpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(scafoldColor),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+            child: Column(
+              children: [
+                CustomAppBar(
+                  title: '',
+                  onTap: () {
+                    context.go(forgetPasswordView);
+                  },
+                ),
+                const SizedBox(height: spacebetweenSections / 2),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Enter Verification Code',
+                    style: AppTextStyles.style24BoldBlack,
+                  ),
+                ),
+                const SizedBox(height: spacebetweenSections / 2),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: RichText(
+                    text: const TextSpan(
+                      text: 'Enter code that we have sent to your\n number ',
+                      style: AppTextStyles.style15SemiBoldGrey,
+                      children: [
+                        TextSpan(
+                          text: '+123 456 7890',
+                          style: AppTextStyles.style16SemiBoldBlack,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: spacebetweenSections),
+                const CustomPinCodeTextField(),
+                const SizedBox(height: spacebetweenSections),
+                CustomButton(
+                  text: 'Verify',
+                  onTap: () {
+                    context.go(createNewPasswordView);
+                  },
+                ),
+                const SizedBox(height: spacebetweenSections),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Didn’t receive the code?  ',
+                      style: AppTextStyles.style15SemiBoldGrey,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        context.go(loginView);
+                      },
+                      child: const Text(
+                        'Resend',
+                        style: AppTextStyles.style15BoldPrimaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
