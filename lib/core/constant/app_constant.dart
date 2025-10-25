6575e86e1e68343fc0899140cd40a7b902c0cad5
Mahmoud Dahy <dahym2028@gmail.com>
2025-10-25 15:15:05 +0300

    add some constant values

--- lib/core/constant/app_constant.dart
+++ lib/core/constant/app_constant.dart
@@ -4,15 +4,26 @@ const int primaryColor = 0xffDD8560;
 const int splashColor = 0xff676767;
 const int scafoldColor = 0xffF4F5F7;
 const int secodaryColor = 0xff4E6542;
+const int fillColorTextField = 0xffF9FAFB;
+const int arrowIconColor = 0xff101623;
+const int iconsColor = 0xffA1A8B0;
 
 const String fontFamily = 'Poppins';
 const String fontFamily2 = 'Montserrat';
+const String fontFamily3 = 'Inter_28pt';
 
 const double defaultPadding = 16.0;
 const double defaultHieghtOnbourding = .7;
 const double defaultCirclerpadding = 24.0;
 const double spacebetweenSections = 32;
+const double iconsSize = 24.0;
 
 const String splash = '/';
 const String root = '/root';
 const String getStarted = '/getStarted';
+const String loginView = '/loginView';
+const String signupView = '/signupView';
+const String forgetPasswordView = '/forgetPassword';
+const String otpView = '/otpView';
+const String createNewPasswordView = '/createNewPasswordView';
+
