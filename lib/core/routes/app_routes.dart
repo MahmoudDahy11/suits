9d008c7c6ab8f06f83dbe0522407acb438695bbd
Mahmoud Dahy <dahym2028@gmail.com>
2025-10-25 15:15:27 +0300

    add new routes

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'package:go_router/go_router.dart';
 import 'package:suits/core/constant/app_constant.dart';
+import 'package:suits/features/auth/presentation/views/create_new_password_view.dart';
+import 'package:suits/features/auth/presentation/views/forget_password_view.dart';
+import 'package:suits/features/auth/presentation/views/login_view.dart';
+import 'package:suits/features/auth/presentation/views/otp_view.dart';
+import 'package:suits/features/auth/presentation/views/signup_view.dart';
 import 'package:suits/features/onboarding/root.dart';
 import 'package:suits/features/onboarding/views/get_started.dart';
 import 'package:suits/features/splash/splash.dart';
@@ -24,6 +29,31 @@ class AppRoutes {
         path: getStarted,
         name: 'getStarted',
         builder: (context, state) => const GetStarted(),
+      ),
+      GoRoute(
+        path: loginView,
+        name: 'loginView',
+        builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: signupView,
+        name: 'signupView',
+        builder: (context, state) => const SignupView(),
+      ),
+      GoRoute(
+        path: forgetPasswordView,
+        name: 'forgetPassword',
+        builder: (context, state) => const ForgetPasswordView(),
+      ),
+      GoRoute(
+        path: otpView,
+        name: 'otpView',
+        builder: (context, state) => const OtpView(),
+      ),
+      GoRoute(
+        path: createNewPasswordView,
+        name: 'createNewPasswordView',
+        builder: (context, state) => const CreateNewPasswordView(),
       ),
     ],
     errorBuilder: (context, state) =>
