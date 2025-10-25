9f9b062d735b1de467877f2a06e3876ed5fd5bc5
Mahmoud Dahy <dahym2028@gmail.com>
2025-10-25 15:15:42 +0300

    add some fonts

--- lib/core/utils/app_text_style.dart
+++ lib/core/utils/app_text_style.dart
@@ -14,15 +14,45 @@ abstract class AppTextStyles {
     fontWeight: FontWeight.w700,
     fontFamily: fontFamily2,
   );
+  static const style24BoldBlack = TextStyle(
+    color: Color(0xff101623),
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    fontFamily: fontFamily3,
+  );
+  static const style18BoldBlack = TextStyle(
+    color: Color(arrowIconColor),
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    fontFamily: fontFamily2,
+  );
   static const style20BoldPrimaryColor = TextStyle(
     color: Color(primaryColor),
     fontSize: 20,
     fontWeight: FontWeight.w700,
     fontFamily: fontFamily2,
   );
+  static const style12BoldPrimaryColor = TextStyle(
+    color: Color(primaryColor),
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    fontFamily: fontFamily3,
+  );
+  static const style15BoldPrimaryColor = TextStyle(
+    color: Color(primaryColor),
+    fontSize: 15,
+    fontWeight: FontWeight.w600,
+    fontFamily: fontFamily3,
+  );
   static const style15SemiBoldGrey = TextStyle(
     color: Color(0xff727272),
-    fontSize: 15,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: fontFamily3,
+  );
+  static const style12SemiBoldBlack = TextStyle(
+    color: Colors.black,
+    fontSize: 12,
     fontWeight: FontWeight.w600,
     fontFamily: fontFamily2,
   );
@@ -31,5 +61,23 @@ abstract class AppTextStyles {
     fontSize: 14,
     fontWeight: FontWeight.w400,
     fontFamily: fontFamily,
+  );
+  static const style14RegularGrey = TextStyle(
+    color: Color(0xff3B4453),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: fontFamily3,
+  );
+  static const style14RegularPrimary = TextStyle(
+    color: Color(primaryColor),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: fontFamily3,
+  );
+  static const style16SemiBoldBlack = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontFamily: fontFamily3,
   );
 }
