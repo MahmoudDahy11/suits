f9049089e2a443faa3d1bd10060d2586f903e17a
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-03 07:01:10 +0200

    create CartView page

---
+++ lib/features/cart/presentation/views/cart_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:suits/core/constant/app_constant.dart';
+
+class CartView extends StatelessWidget {
+  const CartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(backgroundColor: Color(scafoldColor));
+  }
+}
