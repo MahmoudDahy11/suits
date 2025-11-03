815431206003688903fb910ff8ef84e2bcd87a6b
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-03 15:27:24 +0200

    create and finish ui Favorite view

--- lib/features/favorite/presentation/views/fav_view.dart
+++ lib/features/favorite/presentation/views/fav_view.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:suits/core/constant/app_constant.dart';
+import 'package:suits/core/utils/app_text_style.dart';
+
+import 'widgets/grid_product.dart';
+import 'widgets/list_view_category_text_fav.dart';
 
 class FavView extends StatelessWidget {
   const FavView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(backgroundColor: Color(scafoldColor));
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: const Scaffold(
+        backgroundColor: Color(scafoldColor),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: defaultPadding),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "My Wishlist",
+                    style: AppTextStyles.style20BoldBlack,
+                  ),
+                ),
+                SizedBox(height: spacebetweenSections),
+                ListViewCategoryTextFav(),
+                SizedBox(height: spacebetweenSections),
+                Expanded(child: GridProduct()),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
