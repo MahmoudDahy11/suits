5957c88d9282f8fc009f88798b8a99bec1e47ec9
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-03 07:01:57 +0200

    create FavView page

---
+++ lib/features/favorite/presentation/views/fav_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:suits/core/constant/app_constant.dart';
+
+class FavView extends StatelessWidget {
+  const FavView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(backgroundColor: Color(scafoldColor));
+  }
+}
