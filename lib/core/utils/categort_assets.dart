fc2df3ebfc90607fc8f78a2ece4a45a1f9e1f372
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-03 06:55:24 +0200

    add some images and creare category and home assets classes

---
+++ lib/core/utils/categort_assets.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_single_quotes
+class CategoryAssets {
+  CategoryAssets._();
+
+  static const String categoryBlazer = "assets/category/blazer.png";
+
+  static const String categoryMenShoes = "assets/category/men_shoes.png";
+
+  static const String categoryShirt = "assets/category/shirt.png";
+
+  static const String categoryWomanShoes = "assets/category/woman_shoes.png";
+}
---
+++ lib/core/utils/home_assets.dart
@@ -0,0 +1,23 @@
+class HomeAssets {
+  HomeAssets._();
+
+  static const String homeHomeCard = "assets/home/home_card.png";
+
+  static const String homeHomeS1 = "assets/home/home_s1.png";
+
+  static const String homeHomeS2 = "assets/home/home_s2.png";
+
+  static const String homeHomeS3 = "assets/home/home_s3.png";
+
+  static const String homeHomeS4 = "assets/home/home_s4.png";
+
+  static const String homeHomeS5 = "assets/home/home_s5.png";
+
+  static const String homeHomeS6 = "assets/home/home_s6.png";
+
+  static const String homeHomeS7 = "assets/home/home_s7.jpg";
+
+  static const String homeHomeS8 = "assets/home/home_s8.png";
+
+  
+}
