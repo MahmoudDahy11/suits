9c522e383d69881b295ef20d958b8df70964162d
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-03 07:08:05 +0200

    create ProfileView

---
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:suits/core/constant/app_constant.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(scafoldColor),
+    );
+  }
+} 
