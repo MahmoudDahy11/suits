ff5d021ce2565ba9210386324a4bd5bf493cfcac
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-16 22:12:56 +0200

    custom the icon arrow back

--- lib/features/home/presentation/views/widgets/icon_arrow_back.dart
+++ lib/features/home/presentation/views/widgets/icon_arrow_back.dart
@@ -1,20 +1,16 @@
-
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:suits/core/constant/app_constant.dart';
 
 class IconArrowBack extends StatelessWidget {
-  const IconArrowBack({super.key});
-
+  const IconArrowBack({super.key, required this.onTap});
+ final VoidCallback onTap;
   @override
   Widget build(BuildContext context) {
     return Positioned(
       top: 40,
       left: 16,
       child: IconButton(
-        onPressed: () {
-          context.go(homeRoot);
-        },
+        onPressed:onTap,
         icon: const Icon(Icons.arrow_back_ios, color: Color(arrowIconColor)),
       ),
     );
