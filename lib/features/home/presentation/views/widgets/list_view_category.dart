871cf4c1ab0503b76c9be51c07e79a780e1eee9f
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-16 22:09:51 +0200

    handle some logic to prepare api

--- lib/features/home/presentation/views/widgets/list_view_category.dart
+++ lib/features/home/presentation/views/widgets/list_view_category.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:suits/core/constant/app_constant.dart';
 import 'package:suits/core/utils/categort_assets.dart';
 
 class ListViewCategory extends StatelessWidget {
   const ListViewCategory({super.key, this.onTap});
-  final List<String> categories = const [
-    CategoryAssets.categoryBlazer,
-    CategoryAssets.categoryShirt,
-    CategoryAssets.categoryMenShoes,
-    CategoryAssets.categoryWomanShoes,
-    CategoryAssets.categoryBlazer,
-    CategoryAssets.categoryShirt,
+
+  final List<Map<String, String>> categoriesData = const [
+    {'asset': CategoryAssets.categoryBlazer, 'query': 'blazer'},
+    {'asset': CategoryAssets.categoryShirt, 'query': 'shirt'},
+    {'asset': CategoryAssets.categoryMenShoes, 'query': 'men shoes'},
+    {'asset': CategoryAssets.categoryWomanShoes, 'query': 'women shoes'},
   ];
+
   final VoidCallback? onTap;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -20,23 +22,31 @@ class ListViewCategory extends StatelessWidget {
       child: SizedBox(
         height: 50,
         child: ListView.builder(
+          physics: const ClampingScrollPhysics(),
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
+            final categoryItem = categoriesData[index];
+            final categoryQuery = categoryItem['query']!;
+
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: const Color(primaryColor),
-                    width: 1.8,
+              child: GestureDetector(
+                onTap: () =>
+                    context.push('$categoryView?category=$categoryQuery'),
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: const Color(primaryColor),
+                      width: 1.8,
+                    ),
                   ),
+                  child: Image.asset(categoryItem['asset']!),
                 ),
-                child: Image.asset(categories[index]),
               ),
             );
           },
-          itemCount: categories.length,
+          itemCount: categoriesData.length,
         ),
       ),
     );
