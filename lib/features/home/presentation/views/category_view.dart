2d716b55e045f225e54b3e17dafd85cb8d1e358c
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-17 12:26:10 +0200

    refactor the ui

--- lib/features/home/presentation/views/category_view.dart
+++ lib/features/home/presentation/views/category_view.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import 'package:suits/core/constant/app_constant.dart';
 import 'package:suits/core/secret/secret.dart';
 import 'package:suits/core/widgets/custom_app_bar.dart';
 import 'package:suits/features/home/presentation/cubits/get_product/get_product_cubit.dart';
-import 'package:suits/features/home/presentation/views/widgets/card_item.dart';
-
+import '../../data/models/product_model.dart';
+import 'widgets/card_item.dart';
 import '../../domain/entity/product_entity.dart';
+import 'package:suits/features/favorite/presentation/cubits/favorite/favorite_cubit.dart';
 
 class CategoryView extends StatefulWidget {
   final String categoryQuery;
@@ -21,13 +23,14 @@ class CategoryView extends StatefulWidget {
 class _CategoryViewState extends State<CategoryView> {
   @override
   void initState() {
+    super.initState();
     context.read<GetProductCubit>().getProducts(
       queryParameters: {
         'query': widget.categoryQuery,
         'client_id': clientIdApi,
       },
     );
-    super.initState();
+    context.read<FavoriteCubit>().loadFavorites();
   }
 
   @override
@@ -36,6 +39,10 @@ class _CategoryViewState extends State<CategoryView> {
       builder: (context, state) {
         List<ProductEntity> products = [];
         if (state is GetProductSuccess) products = state.products;
+
+        final bool isLoading = state is GetProductLoading;
+        final int itemCount = isLoading ? 6 : products.length;
+
         return Scaffold(
           backgroundColor: const Color(scafoldColor),
           body: SafeArea(
@@ -48,30 +55,74 @@ class _CategoryViewState extends State<CategoryView> {
                     onTap: context.pop,
                   ),
                   const SizedBox(height: spacebetweenSections / 2),
-                  Expanded(
-                    child: GridView.builder(
-                      physics: const BouncingScrollPhysics(),
-                      itemCount: products.length,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            mainAxisSpacing: 12,
-                            crossAxisSpacing: 24,
-                            childAspectRatio: 0.7,
-                          ),
-                      itemBuilder: (context, index) {
-                        return CardItem(
-                          onTap: () {
-                            context.push(
-                              itemDetailsView,
-                              extra: products[index],
-                            );
-                          },
-                          productEntity: products[index],
-                        );
-                      },
+                  if (state is GetProductFailure)
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          state.errMessage,
+                          style: TextStyle(color: Colors.red.shade700),
+                        ),
+                      ),
+                    )
+                  else
+                    Expanded(
+                      child: BlocBuilder<FavoriteCubit, FavoriteState>(
+                        builder: (context, favState) {
+                          return GridView.builder(
+                            physics: const BouncingScrollPhysics(),
+                            itemCount: itemCount,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  mainAxisSpacing: 12,
+                                  crossAxisSpacing: 24,
+                                  childAspectRatio: 0.7,
+                                ),
+                            itemBuilder: (context, index) {
+                              final ProductEntity product;
+                              if (isLoading) {
+                                product = ProductEntity(
+                                  id: index.toString(),
+                                  slug: 'Product Name Skeleton',
+                                  urls: Urls(
+                                    full: '',
+                                    raw: '',
+                                    regular:
+                                        'https://placehold.co/400x600/CCCCCC/AAAAAA/png?text=Loading',
+                                    small: '',
+                                    thumb: '',
+                                    smallS3: '',
+                                  ),
+                                  userName: 'User Skeleton',
+                                  likes: 0,
+                                );
+                              } else {
+                                product = products[index];
+                              }
+
+                              return Skeletonizer(
+                                enabled: isLoading,
+                                effect: ShimmerEffect(
+                                  baseColor: Colors.grey.shade300,
+                                  highlightColor: Colors.grey.shade100,
+                                ),
+                                child: CardItem(
+                                  productEntity: product,
+                                  onTap: isLoading
+                                      ? () {}
+                                      : () {
+                                          context.push(
+                                            itemDetailsView,
+                                            extra: product,
+                                          );
+                                        },
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
                     ),
-                  ),
                 ],
               ),
             ),
