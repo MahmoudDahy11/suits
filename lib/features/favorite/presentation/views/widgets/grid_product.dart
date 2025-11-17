70da4e268ef04b014003c6e06268fd794a00d970
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-17 12:27:21 +0200

    provide fav cubit

--- lib/features/favorite/presentation/views/widgets/grid_product.dart
+++ lib/features/favorite/presentation/views/widgets/grid_product.dart
@@ -1,41 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:suits/core/constant/app_constant.dart';
-import 'package:suits/core/utils/home_assets.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:suits/features/favorite/presentation/cubits/favorite/favorite_cubit.dart';
 import '../../../../home/presentation/views/widgets/card_item.dart';
 
 class GridProduct extends StatelessWidget {
   const GridProduct({super.key});
-  final List<String> images = const [
-    HomeAssets.homeHomeS1,
-    HomeAssets.homeHomeS2,
-    HomeAssets.homeHomeS3,
-    HomeAssets.homeHomeS4,
-    HomeAssets.homeHomeS5,
-    HomeAssets.homeHomeS6,
-    HomeAssets.homeHomeS7,
-    HomeAssets.homeHomeS8,
-  ];
+
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      physics: const BouncingScrollPhysics(),
-      padding: const EdgeInsets.all(16.0),
-      itemCount: images.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 0.7,
-        crossAxisSpacing: 24,
-        mainAxisSpacing: 12,
-      ),
-      itemBuilder: (context, index) {
-        return CardItem(
-          image: images[index],
-          onTap: () {
-            context.go(itemDetailsView);
-          },
-        );
+    return BlocBuilder<FavoriteCubit, FavoriteState>(
+      builder: (context, state) {
+        if (state is FavoriteLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is FavoriteLoaded) {
+          final items = state.items;
+          if (items.isEmpty) {
+            return const Center(child: Text("No favorites yet"));
+          }
+          return GridView.builder(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.all(16.0),
+            itemCount: items.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.7,
+              crossAxisSpacing: 24,
+              mainAxisSpacing: 12,
+            ),
+            itemBuilder: (context, index) {
+              final favoriteItem = items[index];
+
+              return CardItem(
+                productEntity: favoriteItem.product,
+
+                onTap: () {},
+              );
+            },
+          );
+        } else {
+          return const SizedBox.shrink();
+        }
       },
     );
   }
