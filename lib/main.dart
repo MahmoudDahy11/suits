be931132018801f435cbcc9814443cb84a671f5a
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-22 11:00:53 +0200

    provide payment services with paypal and stripe payment

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:suits/core/routes/app_routes.dart';
+import 'package:suits/core/secret/secret.dart';
 import 'package:suits/core/service/get_it.dart';
 import 'package:suits/features/auth/data/service/local_storage.dart';
 import 'package:suits/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = ApiKeys.publishableKey;
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await LocalStorageService.init();
   await getItSetup();
