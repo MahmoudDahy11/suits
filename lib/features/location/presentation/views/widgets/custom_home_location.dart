8dbf3c90d30f2f60674c2a3e9242f20d7b31433b
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-23 18:20:58 +0200

    create CustomHomeLocation

---
+++ lib/features/location/presentation/views/widgets/custom_home_location.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:suits/core/constant/app_constant.dart';
+import 'package:suits/core/utils/app_text_style.dart';
+
+class CustomHomeLocation extends StatelessWidget {
+  const CustomHomeLocation({super.key, required this.address});
+  final String address;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: Color(scafoldColor)),
+      child: Column(
+        children: [
+          const Row(
+            children: [
+              Icon(Icons.location_on_sharp),
+              SizedBox(width: spacebetweenSections / 2),
+              Text('Home', style: AppTextStyles.style20BoldBlack),
+            ],
+          ),
+          const SizedBox(height: spacebetweenSections / 2),
+           Text(
+            address,
+            style: AppTextStyles.style15SemiBoldGrey,
+          ),
+          const SizedBox(height: spacebetweenSections / 2),
+          Divider(thickness: 1.5, color: Colors.grey.shade400),
+        ],
+      ),
+    );
+  }
+}
